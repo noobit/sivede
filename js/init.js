dae2e9768a2375b2bf707a1d7d72d10df5d078f8
noobit <roman.rauch@alumni.tu-berlin.de>
2015-04-02 16:55:43 +0200

    prepend baseurl init.js semicolon

--- js/init.js
+++ js/init.js
@@ -21,17 +21,17 @@
 		reset: 'normalize',
 		containers: '100%',
 		breakpoints: {
-			global: { href: '{{ "/css/style.css" | prepend: site.baseurl }}', grid: { gutters: ['2.5em', 0] } },
-			xlarge: { media: '(max-width: 1800px)', href: '{{ "/css/style-xlarge.css" | prepend: site.baseurl }}' },
-			large: { media: '(max-width: 1280px)', href: '{{ "/css/style-large.css" | prepend: site.baseurl }}', grid: { gutters: ['2em', 0] } },
-			medium: { media: '(max-width: 980px)', href: '{{ "/css/style-medium.css" | prepend: site.baseurl }}'},
-			small: { media: '(max-width: 736px)', href: '{{ "/css/style-small.css" | prepend: site.baseurl }}', grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
-			xsmall: { media: '(max-width: 480px)', href: '{{ "/css/style-xsmall.css" | prepend: site.baseurl }}' }
+			global: { href: "{{ "/css/style.css" | prepend: site.baseurl }}", grid: { gutters: ['2.5em', 0] } },
+			xlarge: { media: '(max-width: 1800px)', href: "{{ "/css/style-xlarge.css" | prepend: site.baseurl }}" },
+			large: { media: '(max-width: 1280px)', href: "{{ "/css/style-large.css" | prepend: site.baseurl }}", grid: { gutters: ['2em', 0] } },
+			medium: { media: '(max-width: 980px)', href: "{{ "/css/style-medium.css" | prepend: site.baseurl }}"},
+			small: { media: '(max-width: 736px)', href: "{{ "/css/style-small.css" | prepend: site.baseurl }}", grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
+			xsmall: { media: '(max-width: 480px)', href: "{{ "/css/style-xsmall.css" | prepend: site.baseurl }}" }
 		}
 	});
 
 	$(function() {
-		
+	
 		var $window = $(window),
 			$body = $('body'),
 			$header = $('#header');
